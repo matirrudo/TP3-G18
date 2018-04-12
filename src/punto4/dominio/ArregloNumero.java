f95b00980a13f64496ffdedc212883d3d2f62b91
Patagua Ivan <PC1@PC1-PC>
2018-04-12 09:54:43 -0300

    modifique el punto 4 asi que ya esta completo

--- src/punto4/dominio/ArregloNumero.java
+++ src/punto4/dominio/ArregloNumero.java
@@ -3,7 +3,7 @@ package punto4.dominio;
 
 public class ArregloNumero {
     
-    private int[] numeros;
+    private int[] numeros=new int [10];
 
     public int[] getNumeros() {
         return numeros;
@@ -17,5 +17,33 @@ public class ArregloNumero {
         for(int i=0;i<numeros.length;i++){
             System.out.println("Valor de elemento: "+numeros[i]);
         }
+    }
+    
+    public int obtenerMayor(){
+        int mayor=numeros[0];
+        for(int i=0;i<numeros.length;i++){
+            if(numeros[i]>mayor){
+                mayor=numeros[i];
+            }
+        }
+        return mayor;
+    }
+    
+    public int obtenerMenor(){
+        int menor=numeros[0];
+        for(int i=0;i<numeros.length;i++){
+            if(numeros[i]<menor){
+                menor=numeros[i];
+            }
+        }
+        return menor;
+    }
+    
+    public double obtenerPromedio(){
+        int suma=0;
+        for(int i=0;i<numeros.length;i++){
+            suma=suma+numeros[i];
+        }
+        return suma/10;
     }
 }
