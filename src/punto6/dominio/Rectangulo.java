5e1b4e8957719e37797b36d48714379aac91e4e4
Jona255 <38327151+Jona255@users.noreply.github.com>
2018-04-14 21:59:54 -0300

    Update Rectangulo.java
    resscribi la clase Rectangulo

--- src/punto6/dominio/Rectangulo.java
+++ src/punto6/dominio/Rectangulo.java
@@ -1,55 +1,121 @@
 package punto6.dominio;
 
+
 public class Rectangulo {
     private double x;
     private double y;
     private double base;
     private double altura;
     private double sup;
-    private double per;
+    private double per;    
+    private String nombre;    
     
-    
-    public Rectangulo(double x,double y, double base,double altura){
-        this.x=x;
-        this.y=y;
-        this.base=base;
-        this.altura=altura;
+
+    public Rectangulo(String nombre, double x, double y, double base, double altura) {
+        this.x = x;
+        this.y = y;
+        this.base = base;
+        this.altura = altura;
+        this.nombre = nombre;
     }
 
+    
+    
+    /**
+     * @return the x
+     */
     public double getX() {
         return x;
     }
+
+    /**
+     * @param x the x to set
+     */
     public void setX(double x) {
         this.x = x;
     }
+
+    /**
+     * @return the y
+     */
     public double getY() {
         return y;
     }
+
+    /**
+     * @param y the y to set
+     */
     public void setY(double y) {
         this.y = y;
     }
+
+    /**
+     * @return the base
+     */
     public double getBase() {
         return base;
     }
+
+    /**
+     * @param base the base to set
+     */
     public void setBase(double base) {
         this.base = base;
     }
+
+    /**
+     * @return the altura
+     */
     public double getAltura() {
         return altura;
     }
+
+    /**
+     * @param altura the altura to set
+     */
     public void setAltura(double altura) {
         this.altura = altura;
     }
+
+    /**
+     * @return the sup
+     */
     public double getSup() {
         return sup;
     }
+
+    /**
+     * @param sup the sup to set
+     */
     public void setSup(double sup) {
-        this.sup = base*altura;
+        this.sup = sup;
     }
+
+    /**
+     * @return the per
+     */
     public double getPer() {
         return per;
     }
+
+    /**
+     * @param per the per to set
+     */
     public void setPer(double per) {
-        this.per = base*2+altura*2;
+        this.per = per;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
