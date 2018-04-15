9d552a25fea2478a7343b6a59d7eef4421268359
Jona255 <38327151+Jona255@users.noreply.github.com>
2018-04-14 23:29:07 -0300

    Casi listo, solo tengo un problema con la logica
    el problema es que se elimina por index y cuando se "elimina" uno se reasigna a esa posicion quitando el ultimo index, por ende si se quiere eliminar el ultimo index tira error (por ejemplo de los 3 rectangulos, ingreso el numero 1 y lo elimina, pero cuando quiero eliminar el 3, tira error porque no existe esa posicion) (en la variable int "rectangulo" guardo el numero ingresado restandole 1, para enviar bien el index)

--- src/punto6/inicio/Principal.java
+++ src/punto6/inicio/Principal.java
@@ -1,26 +1,51 @@
 package punto6.inicio;
 
+import java.util.Scanner;
 import punto6.dominio.GestorRectangulos;
 import punto6.dominio.Rectangulo;
 
 public class Principal {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Rectangulo rec1=new Rectangulo("rectangulo1",1, 2, 2, 4);
-        Rectangulo rec2=new Rectangulo("rectangulo2",5, 5, 2, 4);
-        Rectangulo rec3=new Rectangulo("rectangulo2",10, 10, 2, 4);
+        Rectangulo rec2=new Rectangulo("rectangulo2",5, 5, 3, 6);
+        Rectangulo rec3=new Rectangulo("rectangulo3",10, 10, 4, 8);
         
         GestorRectangulos gestor =new GestorRectangulos(rec1);
+        
         gestor.agregarRectangulo(rec1);
         gestor.agregarRectangulo(rec2);
         gestor.agregarRectangulo(rec3);
+        
         for (Rectangulo asd: gestor.getListaRectangulos()) {
-            System.out.println("nombre: "+asd.getNombre());
-            System.out.println("punto x: "+asd.getX());
-            System.out.println("punto y: "+asd.getY());
-            System.out.println("base: "+asd.getBase());
-            System.out.println("altura: "+asd.getAltura());
-            System.out.println("----------");    
+            System.out.println("NOMBRE: "+asd.getNombre());
+            System.out.println("punto x: "+asd.getX()+", punto y: "+asd.getY());
+            System.out.println("base: "+asd.getBase()+", altura: "+asd.getAltura());
+            System.out.println("perimetro: "+asd.getPer()+", superficie: "+asd.getSup());
+            System.out.println("-----------------");    
+            
+        }
+        boolean band=true;
+        int rectangulo;
+        while (band==true) {            
+            System.out.println("Que rectangulo quiere eliminar? (1, 2 o 3): ");
+            System.out.println("Para terminar ingrese 0: ");
+            rectangulo=sc.nextInt();
+            if (rectangulo==1||rectangulo==2||rectangulo==3) {
+                gestor.eliminarRectangulo(rectangulo-1);   
+                System.out.println("SE A ELIMINADO EL RECTANGULO "+(rectangulo+1));
+            }else{
+                band=false;
+            }
+            for (Rectangulo asd: gestor.getListaRectangulos()) {
+            System.out.println("NOMBRE: "+asd.getNombre());
+            System.out.println("punto x: "+asd.getX()+", punto y: "+asd.getY());
+            System.out.println("base: "+asd.getBase()+", altura: "+asd.getAltura());
+            System.out.println("perimetro: "+asd.getPer()+", superficie: "+asd.getSup());
+            System.out.println("-----------------");    
+            
+        }
         }
     }
     
