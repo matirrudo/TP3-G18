f403d03f95a87f77d8cd0260705311f8d09270bf
Jona255 <38327151+Jona255@users.noreply.github.com>
2018-04-14 22:01:44 -0300

    REESCRIBI EL PUNTO 6...
    Volvi a reescribir todo el codigo del punto 6 que estaba mal hecho, igual me siguen faltando un par de cosas para terminarlo

--- src/punto6/inicio/Principal.java
+++ src/punto6/inicio/Principal.java
@@ -6,13 +6,22 @@ import punto6.dominio.Rectangulo;
 public class Principal {
 
     public static void main(String[] args) {
-        Rectangulo rec1 = new Rectangulo(1, 2, 2, 4);
-        Rectangulo rec2 = new Rectangulo(12, 10, 4, 6);
-        Rectangulo rec3 = new Rectangulo(10, 20, 6, 8);
-
-        GestorRectangulos gestor = new GestorRectangulos();
+        Rectangulo rec1=new Rectangulo("rectangulo1",1, 2, 2, 4);
+        Rectangulo rec2=new Rectangulo("rectangulo2",5, 5, 2, 4);
+        Rectangulo rec3=new Rectangulo("rectangulo2",10, 10, 2, 4);
+        
+        GestorRectangulos gestor =new GestorRectangulos(rec1);
         gestor.agregarRectangulo(rec1);
-        gestor.mostrarRectangulosYDatos();
+        gestor.agregarRectangulo(rec2);
+        gestor.agregarRectangulo(rec3);
+        for (Rectangulo asd: gestor.getListaRectangulos()) {
+            System.out.println("nombre: "+asd.getNombre());
+            System.out.println("punto x: "+asd.getX());
+            System.out.println("punto y: "+asd.getY());
+            System.out.println("base: "+asd.getBase());
+            System.out.println("altura: "+asd.getAltura());
+            System.out.println("----------");    
+        }
     }
-
+    
 }
